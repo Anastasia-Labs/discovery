c64dcd49b64337b8b2a5341ba88b4860900dbfde
Philip DiSarro <philipdisarro@gmail.com>
2024-03-21 14:53:49 -0700

    traces

--- app/Main.hs
+++ app/Main.hs
@@ -57,7 +57,7 @@ evalT x = evalWithArgsT x []
 
 evalWithArgsT :: ClosedTerm a -> [Data] -> Either Text (Script, ExBudget, [Text])
 evalWithArgsT x args = do
-  cmp <- compile (Config DoTracingAndBinds) x
+  cmp <- compile (Config DoTracing) x
   let (escr, budg, trc) = evalScript $ applyArguments cmp args
   scr <- first (pack . show) escr
   pure (scr, budg, trc)
--- src/PriceDiscoveryEvent/MultiFoldLiquidity.hs
+++ src/PriceDiscoveryEvent/MultiFoldLiquidity.hs
@@ -578,7 +578,7 @@ prewardSuccessor nodeCS projectCS projectTN totalProjectTokens totalCommitted st
       successorChecks = 
         pand'List 
           [ ptraceIfFalse "rck" (accNodeF.next #== nodeKey)
-          , ptraceIfFalse ("rcv" <> (pshow nodeOutputF.value)) $ (inputValue <> owedProjectValue <> owedAdaValue) #== pforgetPositive nodeOutputF.value
+          , ptraceIfFalse ("rcv" <> (pshow ((inputValue <> owedProjectValue <> owedAdaValue)))) $ (inputValue <> owedProjectValue <> owedAdaValue) #== pforgetPositive nodeOutputF.value
           , ptraceIfFalse "rca" $ nodeOutputF.address #== nodeInputF.address 
           , ptraceIfFalse "rcd" $ nodeOutputF.datum #== nodeInputF.datum 
           , ptraceIfFalse "rct" $ pvalueOfOneScott # nodeCS # inputValue
